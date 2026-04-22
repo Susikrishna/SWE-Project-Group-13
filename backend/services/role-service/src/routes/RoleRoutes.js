83fe10f42ce9539afe8ec3360784a2de0afcc46c
Aditya Kolgane <166222445+adityak2006@users.noreply.github.com>
2026-04-22 23:32:32 +0530

    Refactor role editing route to use editRole

--- backend/services/role-service/src/routes/RoleRoutes.js
+++ backend/services/role-service/src/routes/RoleRoutes.js
@@ -1,14 +1,12 @@
-const express = require("express");
-const router = express.Router();
 const { createRole } = require("../controllers/createRole");
 const {getRoleById, getRoles} = require("../controllers/getRole") 
 const {deleteRoleByID, deleteRoles} = require("../controllers/deleteRole")
-const {editRoleById} = require("../controllers/editRole")
+const {editRole} = require("../controllers/editRole")
 router.get("/", getRoles);
 router.get("/:id", getRoleById);
 router.post("/", createRole);
 router.delete("/",deleteRoleByID)
 router.delete("/del", deleteRoles)
-router.put("/",editRoleById)
+router.put("/", editRole)
 
 module.exports = router;
